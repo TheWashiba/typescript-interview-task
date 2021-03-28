cc364dca0b026a025013a812a5756e4b9ffa6249
TheWashiba <lukas.bobinas@gmail.com>
2021-03-28 18:07:19 +0300

    constants - logout api route added

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,14 @@
 export enum Routes {
-  Login = '/login',
-  PasswordHealth = '/items',
-  Weak = '/items/weak',
-  Reused = '/items/reused',
-  Root = '/',
+  Login = "/login",
+  PasswordHealth = "/items",
+  Weak = "/items/weak",
+  Reused = "/items/reused",
+  Root = "/",
 }
 
 export enum API {
-  Login = 'api/login',
-  Items = 'api/items',
-  User = 'api/user',
+  Login = "api/login",
+  Logout = "api/logout",
+  Items = "api/items",
+  User = "api/user",
 }

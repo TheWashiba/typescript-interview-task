f22dbdabfa955ebf8f198e7c817dc87030c7b35e
TheWashiba <lukas.bobinas@gmail.com>
2021-04-01 23:04:43 +0300

    PasswordHealth route items memoized

--- src/components/PasswordHealth/PasswordHealth.tsx
+++ src/components/PasswordHealth/PasswordHealth.tsx
@@ -12,6 +12,7 @@ import {
   itemIs30DaysOld,
 } from '~/utils';
 import { useUserItemsContext } from '../shared/UserItemsContext';
+import { useMemo } from 'react';
 
 const PasswordHealth = () => {
   const {
@@ -20,6 +21,12 @@ const PasswordHealth = () => {
     username,
   } = useUserContext();
   const { items, isLoading, errorMessage } = useUserItemsContext();
+  const weakItem = useMemo(() => items.filter(itemHasWeakPassword), [items]);
+  const reusedItems = useMemo(
+    () => items.filter((item) => itemHasReusedPassword(item, items)),
+    [items]
+  );
+  const oldItems = useMemo(() => items.filter(itemIs30DaysOld), [items]);
 
   if (isLoading || userDataIsLoading) {
     return <LoadingScreen />;
@@ -38,15 +45,13 @@ const PasswordHealth = () => {
           <List items={items} />
         </Route>
         <Route path={Routes.Weak}>
-          <List items={items.filter(itemHasWeakPassword)} />
+          <List items={weakItem} />
         </Route>
         <Route path={Routes.Reused}>
-          <List
-            items={items.filter((item) => itemHasReusedPassword(item, items))}
-          />
+          <List items={reusedItems} />
         </Route>
         <Route path={Routes.Old}>
-          <List items={items.filter(itemIs30DaysOld)} />
+          <List items={oldItems} />
         </Route>
       </Switch>
     </div>
